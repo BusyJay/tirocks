ed9b74f77b2b47d52cf73a500987ba900bab81ae
Jay Lee <BusyJayLee@gmail.com>
2022-01-22 21:00:34 +0800

    link bzip2
    Signed-off-by: Jay Lee <BusyJayLee@gmail.com>

--- tirocks-sys/src/lib.rs
+++ tirocks-sys/src/lib.rs
@@ -3,6 +3,11 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
+
+// bzip2 requires a user-defined `bz_internal_error` hook to handle errors
+// as it's an alloc free library. bzip2_sys provides a default implementation.
+extern crate bzip2_sys;
+
 #[allow(clippy::all)]
 mod bindings {
     include!(env!("BINDING_PATH"));
