5a9f35901b0e2f6175dbf42fd0d8e40d6f1e80d8
Jay Lee <BusyJayLee@gmail.com>
2022-07-04 23:03:49 -0700

    add open options
    This PR adds 3 types of open options for building db:
    - Open db with the default cf only,
    - Open db with multiple cfs,
    - Open titan db with multiple cfs.
    
    Signed-off-by: Jay Lee <BusyJayLee@gmail.com>

--- tirocks/src/db.rs
+++ tirocks/src/db.rs
@@ -2,6 +2,8 @@
 
 mod cf;
 mod imp;
+mod open_options;
 
 pub use cf::{CfHandle, RawCfHandle, DEFAULT_CF_NAME};
 pub use imp::{Db, RawDb, RawDbRef};
+pub use open_options::{DefaultCfOnlyBuilder, MultiCfBuilder, MultiCfTitanBuilder, OpenOptions};
